8f54a8f9564b17f5a24c9e40ef848a473b6899b5
Lhy1122 <1319628965@qq.com>
2022-02-14 16:57:02 +0800

    new

--- Main.java
+++ Main.java
@@ -1,14 +1,15 @@
 /*
  * @Author: your name
  * @Date: 2022-02-10 21:48:37
- * @LastEditTime: 2022-02-14 15:56:28
+ * @LastEditTime: 2022-02-14 16:54:00
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \java文件\01\Main.java
  * 
  * 
- * git status
-
+ * git status 查看git 文件状态
+ * 
+ * 
  */
 
 
