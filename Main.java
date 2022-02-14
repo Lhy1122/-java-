fe34c9aa7c90dff4b01603d4055a7ea930b45677
日本天皇 <99628172+Lhy1122@users.noreply.github.com>
2022-02-14 15:07:33 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,63 @@
+/*
+ * @Author: your name
+ * @Date: 2022-02-10 21:48:37
+ * @LastEditTime: 2022-02-13 18:35:16
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \java文件\01\Main.java
+ */
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Array<Integer> www = new Array<>(5);
+        www.addFirst(114);
+
+        www.addLast(810);
+
+        www.add(1,514);
+
+        www.add(2,1919);
+
+        www.addLast(9527);
+
+        www.addLast(12);
+        System.out.println(www);
+
+        www.deleteLast();
+        System.out.println(www);
+
+        /*for(int i = 0; i < www.getsize(); i++){
+            System.out.println(www.get(i));
+        }
+
+        System.out.println(www);
+
+        System.out.println(www.get(0));
+
+        www.set(2, 1919);
+        System.out.println(www);
+
+        System.out.println(www.contains(514));
+
+        System.out.println(www.findindex(114));
+
+        www.delete(1);
+        System.out.println(www);
+
+        www.add(1,514);
+        www.addLast(810);
+        System.out.println(www);
+
+        www.deleteFirst();
+        System.out.println(www.deleteLast());
+        System.out.println(www);
+
+        www.deleteElement(514);
+        */
+        
+
+    }
+
+}
