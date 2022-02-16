ec46179574ee43c714edf30af0d0bfb3d9d1b8d5
Lhy1122 <1319628965@qq.com>
2022-02-16 12:40:49 +0800

    03

---
+++ 03-Stacks-and-Queues/03-A-Stack-Problem-in-Leetcode/Solution.java
@@ -0,0 +1,33 @@
+/*
+ * @Author: your name
+ * @Date: 2022-02-16 11:16:18
+ * @LastEditTime: 2022-02-16 12:38:14
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \java-algorithm\03-Stacks-and-Queues\03-A-Stack-Problem-in-Leetcode\Solution.java
+ */
+import java.util.Stack;
+
+public class Solution {
+    public boolean isValid(String s){
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            if(c == '(' || c == '[' || c == '{')
+                stack.push(c);
+            else{
+                if(stack.isEmpty())
+                    return false;
+                
+                char topChar = stack.pop();
+                if(c == ')' && topChar != '(')
+                    return false;
+                if(c == ']' && topChar != '[')
+                    return false;
+                if(c == '}' && topChar != '{')
+                    return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
