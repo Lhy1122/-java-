1406f6f194a8d0fabf594b9ad940610b5bf2c274
Lhy1122 <1319628965@qq.com>
2022-02-28 22:08:58 +0800

    study

--- 05-Recursion/06-Debug-Recursive-Solution/Solution.java
+++ 05-Recursion/06-Debug-Recursive-Solution/Solution.java
@@ -1,11 +1,56 @@
 /*
  * @Author: your name
  * @Date: 2022-02-28 22:00:05
- * @LastEditTime: 2022-02-28 22:00:05
- * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2022-02-28 22:07:53
+ * @LastEditors: your name
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \java-algorithm\05-Recursion\06-Debug-Recursive-Solution\Solution.java
  */
 public class Solution {
-    
+
+    public ListNode removeElements(ListNode head, int val, int depth) {
+        String depthString = generateDepthString(depth);
+
+        System.out.print(depthString);
+        System.out.println("Call: remove " + val + " in " + head);
+
+        if(head == null){
+            System.out.print(depthString);
+            System.out.println("Return: " + head);
+            return head;
+        }
+
+        ListNode res = removeElements(head.next, val, depth + 1);
+        System.out.print(depthString);
+        System.out.println("After remove " + val + ": " + res);
+
+        ListNode ret;
+        if(head.val == val)
+            ret = res;
+        else{
+            head.next = res;
+            ret = head;
+        }
+        System.out.print(depthString);
+        System.out.println("Return: " + ret);
+
+        return ret;
+    }
+
+    private String generateDepthString(int depth){
+        StringBuilder res = new StringBuilder();
+        for(int i = 0 ; i < depth ; i ++)
+            res.append("--");
+        return res.toString();
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 6, 3, 4, 5, 6};
+        ListNode head = new ListNode(nums);
+        System.out.println(head);
+
+        ListNode res = (new Solution()).removeElements(head, 6, 0);
+        System.out.println(res);
+    }
+
 }
