4ebddda1b4523fda8f635cfeca39ed4332447608
Lhy1122 <1319628965@qq.com>
2022-03-02 23:24:42 +0800

    study

---
+++ 06-Binary-Search-Tree/02-Binary-Search-Tree-Basics/BST.java
@@ -0,0 +1,11 @@
+public class BST {
+    
+}
+/*
+ * @Author: your name
+ * @Date: 2022-03-02 23:23:36
+ * @LastEditTime: 2022-03-02 23:23:36
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \java-algorithm\06-Binary-Search-Tree\02-Binary-Search-Tree-Basics\BST.java
+ */
---
+++ 06-Binary-Search-Tree/02-Binary-Search-Tree-Basics/Main.java
@@ -0,0 +1,13 @@
+/*
+ * @Author: your name
+ * @Date: 2022-03-02 23:22:27
+ * @LastEditTime: 2022-03-02 23:22:28
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \java-algorithm\06-Binary-Search-Tree\02-Binary-Search-Tree-Basics\Main.java
+ */
+public class Main {
+    public static void main(String[] args){
+        
+    }
+}
