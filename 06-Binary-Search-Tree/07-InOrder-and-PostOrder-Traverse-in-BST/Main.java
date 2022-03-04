6d77addecfaa62820b152195685bbf0718ef54e1
Lhy1122 <1319628965@qq.com>
2022-03-04 22:45:33 +0800

    study

--- 06-Binary-Search-Tree/07-InOrder-and-PostOrder-Traverse-in-BST/Main.java
+++ 06-Binary-Search-Tree/07-InOrder-and-PostOrder-Traverse-in-BST/Main.java
@@ -1,11 +1,26 @@
-public class Main {
-    
-}
 /*
  * @Author: your name
  * @Date: 2022-03-04 22:38:55
- * @LastEditTime: 2022-03-04 22:38:55
- * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2022-03-04 22:44:42
+ * @LastEditors: your name
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \java-algorithm\06-Binary-Search-Tree\07-InOrder-and-PostOrder-Traverse-in-BST\Main.java
  */
+public class Main {
+    public static void main(String[] args) {
+
+        BST<Integer> bst = new BST<>();
+        int[] nums = {5, 3, 6, 8, 4, 2};
+        for(int num: nums)
+            bst.add(num);
+
+        bst.preOrder();
+        System.out.println();
+
+        bst.inOrder();
+        System.out.println();
+
+        bst.postOrder();
+        System.out.println();
+    }
+}
