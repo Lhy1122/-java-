8ad8f506973a8025e1db2d5e57f53f0a44ee4860
Lhy1122 <1319628965@qq.com>
2022-03-09 22:53:20 +0800

    study

---
+++ 07-Set-and-Map/01-Set-Basics-and-BSTSet/BSTSet.java
@@ -0,0 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2022-03-09 22:12:56
+ * @LastEditTime: 2022-03-09 22:12:57
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \java-algorithm\07-Set-and-Map\01-Set-Basics-and-BSTSet\BSTSet.java
+ */
+public class BSTSet {
+    
+}
---
+++ 07-Set-and-Map/01-Set-Basics-and-BSTSet/FileOperation.java
@@ -0,0 +1,69 @@
+/*
+ * @Author: your name
+ * @Date: 2022-03-09 22:19:02
+ * @LastEditTime: 2022-03-09 22:21:36
+ * @LastEditors: your name
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \java-algorithm\07-Set-and-Map\01-Set-Basics-and-BSTSet\FileOperation.java
+ */
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Locale;
+import java.io.File;
+import java.io.BufferedInputStream;
+import java.io.IOException;
+
+public class FileOperation {
+
+    public static boolean readFile(String filename, ArrayList<String> words){
+
+        if (filename == null || words == null){
+            System.out.println("filename is null or words is null");
+            return false;
+        }
+
+        Scanner scanner;
+
+        try {
+            File file = new File(filename);
+            if(file.exists()){
+                FileInputStream fis = new FileInputStream(file);
+                scanner = new Scanner(new BufferedInputStream(fis), "UTF-8");
+                scanner.useLocale(Locale.ENGLISH);
+            }
+            else
+                return false;
+        }
+        catch(IOException ioe){
+            System.out.println("Cannot open " + filename);
+            return false;
+        }
+
+        if (scanner.hasNextLine()) {
+
+            String contents = scanner.useDelimiter("\\A").next();
+
+            int start = firstCharacterIndex(contents, 0);
+            for (int i = start + 1; i <= contents.length(); )
+                if (i == contents.length() || !Character.isLetter(contents.charAt(i))) {
+                    String word = contents.substring(start, i).toLowerCase();
+                    words.add(word);
+                    start = firstCharacterIndex(contents, i);
+                    i = start + 1;
+                } else
+                    i++;
+        }
+
+        return true;
+    }
+
+    
+    private static int firstCharacterIndex(String s, int start){
+
+        for( int i = start ; i < s.length() ; i ++ )
+            if( Character.isLetter(s.charAt(i)) )
+                return i;
+        return s.length();
+    }
+}
