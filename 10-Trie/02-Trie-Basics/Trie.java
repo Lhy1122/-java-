5567b6ab35714d3d4acc27a5443a85444744c09a
Lhy1122 <1319628965@qq.com>
2022-03-26 21:57:39 +0800

    study

---
+++ 10-Trie/02-Trie-Basics/Trie.java
@@ -0,0 +1,54 @@
+/*
+ * @Author: your name
+ * @Date: 2022-03-26 21:09:14
+ * @LastEditTime: 2022-03-26 21:56:25
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \java-algorithm\Trie\02-Trie-Basics\Trie.java
+ */
+
+import java.util.TreeMap;
+
+public class Trie {
+    private class Node{
+        public boolean isWord;
+        public TreeMap<Character, Node> next;
+
+        public Node(boolean isWord){
+            this.isWord = isWord;
+            next = new TreeMap<>();
+        }
+
+        public Node(){
+            this(false);
+        }
+    }
+
+    private Node root;
+    private int size;
+
+    public Trie(){
+        root = new Node();
+        size = 0;
+    }
+
+    public int getSize(){
+        return size;
+    }
+
+    public void add(String word){
+        Node cur = root;
+        for(int i = 0; i < word.length(); i++){
+            char c = word.charAt(i);
+            if(cur.next.get(c) == null)
+                cur.next.put(c, new Node());
+            cur = cur.next.get(c);
+        }
+
+        if(!cur.isWord){
+            cur.isWord = true;
+            size++;
+        }
+    }
+    
+}
