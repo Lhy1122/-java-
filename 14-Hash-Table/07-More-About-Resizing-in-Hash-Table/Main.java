ff1c70039ddd9c33d30e2005f6679ca106e99898
Lhy1122 <1319628965@qq.com>
2022-04-30 21:31:39 +0800

    study

---
+++ 14-Hash-Table/07-More-About-Resizing-in-Hash-Table/Main.java
@@ -0,0 +1,98 @@
+/*
+ * @Author: your name
+ * @Date: 2022-04-30 21:30:06
+ * @LastEditTime: 2022-04-30 21:30:07
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \java-algorithm\14-Hash-Table\07-More-About-Resizing-in-Hash-Table\Main.java
+ */
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Pride and Prejudice");
+
+        ArrayList<String> words = new ArrayList<>();
+        if(FileOperation.readFile("pride-and-prejudice.txt", words)) {
+            System.out.println("Total words: " + words.size());
+
+            long startTime = System.nanoTime();
+
+            BST<String, Integer> bst = new BST<>();
+            for (String word : words) {
+                if (bst.contains(word))
+                    bst.set(word, bst.get(word) + 1);
+                else
+                    bst.add(word, 1);
+            }
+
+            for(String word: words)
+                bst.contains(word);
+
+            long endTime = System.nanoTime();
+
+            double time = (endTime - startTime) / 1000000000.0;
+            System.out.println("BST: " + time + " s");
+
+            startTime = System.nanoTime();
+
+            AVLTree<String, Integer> avl = new AVLTree<>();
+            for (String word : words) {
+                if (avl.contains(word))
+                    avl.set(word, avl.get(word) + 1);
+                else
+                    avl.add(word, 1);
+            }
+
+            for(String word: words)
+                avl.contains(word);
+
+            endTime = System.nanoTime();
+
+            time = (endTime - startTime) / 1000000000.0;
+            System.out.println("AVL: " + time + " s");
+
+
+            startTime = System.nanoTime();
+
+            RBTree<String, Integer> rbt = new RBTree<>();
+            for (String word : words) {
+                if (rbt.contains(word))
+                    rbt.set(word, rbt.get(word) + 1);
+                else
+                    rbt.add(word, 1);
+            }
+
+            for(String word: words)
+                rbt.contains(word);
+
+            endTime = System.nanoTime();
+
+            time = (endTime - startTime) / 1000000000.0;
+            System.out.println("RBTree: " + time + " s");
+
+
+            startTime = System.nanoTime();
+
+            HashTable<String, Integer> ht = new HashTable<>();
+            for (String word : words) {
+                if (ht.contains(word))
+                    ht.set(word, ht.get(word) + 1);
+                else
+                    ht.add(word, 1);
+            }
+
+            for(String word: words)
+                ht.contains(word);
+
+            endTime = System.nanoTime();
+
+            time = (endTime - startTime) / 1000000000.0;
+            System.out.println("HashTable: " + time + " s");
+        }
+
+        System.out.println();
+    }
+}
